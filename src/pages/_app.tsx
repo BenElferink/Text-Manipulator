cdcf216b5a18ed7f5f87b2a6278648d1cb9d81c0
Ben Elferink <ben.elferink@icloud.com>
2024-04-26 10:03:38 +0300

    init app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import { Fragment } from 'react'
+import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Fragment>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='author' content='Ben Elferink' />
+        <meta name='description' content='Manipulate text strings' />
+
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/manifest.json' />
+
+        <title>Text Manipulator</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
+
+export default App
